fab3ef1741ce8c3d509185d8f1934bd9c70b4b39
Cristopher <cristophervaldezd@gmail.com>
2025-05-21 21:53:34 -0400

    testing

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { useQuery } from '@apollo/client';
 
 import { QUERY_PROFILES } from '../utils/queries';
 
- import'./Home.css;
+ import'./Home.css';
 const Home = () => {
   const { loading, data } = useQuery(QUERY_PROFILES);
   const profiles = data?.profiles || [];
