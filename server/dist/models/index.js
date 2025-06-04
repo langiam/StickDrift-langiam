dd569b444ce11d4f3dc685ba5ba95e4db189ee9b
langiam <ryan.matthew.lang@gmail.com>
2025-06-04 14:54:18 -0400

    chore: remove server/dist from repo and update .gitignore

--- server/dist/models/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// server/src/models/index.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Profile = void 0;
-var Profile_1 = require("./Profile");
-Object.defineProperty(exports, "Profile", { enumerable: true, get: function () { return Profile_1.Profile; } });
