b58344039040d208b3f3732532738d9eb3b91e84
langiam <ryan.matthew.lang@gmail.com>
2025-06-04 17:14:32 -0400

    fixed Lint issue

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,43 +1,43 @@
-// client/.eslintrc.cjs
+/**
+ * client/.eslintrc.cjs
+ *
+ * − Move any custom rule entries out of “extends” and into a top‐level “rules” object.
+ * − Make sure “react/react‐in‐jsx‐scope” is turned off so you don’t need to import React in every JSX file.
+ */
 
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020, 
+    ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
+
   plugins: ['@typescript-eslint', 'react'],
+
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
-  settings: {
-    react: {
-      // “automatic” runtime lets you use JSX without importing React
-      version: 'detect',
-      pragma: 'React',
-      fragment: 'Fragment',
-    },
-  },
+
   rules: {
-    // 1) Turn off “React must be in scope” for JSX:
+    // We’re using React 17+ with the “react-jsx” runtime, so we don’t need React in scope.
     'react/react-in-jsx-scope': 'off',
 
-    // 2) If you want to allow “any” temporarily, lower the severity:
+    // Allow “any” for now, but only as a warning:
     '@typescript-eslint/no-explicit-any': 'warn',
 
-    // 3) (Optional) If you don’t want to see unused-vars for React components:
+    // Warn on unused variables, but allow variables named “React” or args prefixed with “_”
     '@typescript-eslint/no-unused-vars': [
       'warn',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^React$' },
     ],
 
-    // You can add any other custom rules here…
+    // (You can add more custom rules here if needed.)
   },
 };
