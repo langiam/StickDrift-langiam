ea537c70a99d89b613f32156543262e36ddda0f7
langiam <ryan.matthew.lang@gmail.com>
2025-06-08 13:56:52 -0400

    added game search bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,33 @@
-// client/src/App.tsx
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
-import { Outlet } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SearchBar from './components/SearchBar';
+import GameSearchBar from './components/GameSearchBar';
 
-// Apollo setup
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import GameView from './pages/GameView';
+import Wishlist from './pages/Wishlist';
+import Library from './pages/Library';
+import Playlist from './pages/Playlist';
+import Profile from './pages/Profile';
+import Search from './pages/Search'; // This renders RAWG search results
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:3001/graphql',
   credentials: 'include',
@@ -32,14 +51,28 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <SearchBar />
-        <div className="container">
-          <Outlet /> {/* This is where routed pages render */}
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <SearchBar />
+          <GameSearchBar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/game/:id" element={<GameView />} />
+              <Route path="/wishlist" element={<Wishlist />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/playlist" element={<Playlist />} />
+              <Route path="/profile/:profileId" element={<Profile />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
---
+++ client/src/components/GameSearchBar/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './GameSearchBar.css';
+
+const GameSearchBar = () => {
+  const [input, setInput] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const query = input.trim();
+    if (query) {
+      navigate(`/search?query=${encodeURIComponent(query)}`);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="game-search-form">
+      <input
+        type="text"
+        name="query"
+        className="game-search-input"
+        placeholder="Search games..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit" className="game-search-button">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default GameSearchBar;
